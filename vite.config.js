4f1775c5790920a9f6544532cb47f99d9e5b0e14
Will <willtmatteson@gmail.com>
2024-01-25 02:16:59 -0800

    Updating

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/personalwebsite/',
+  //base: '/personalwebsite/',
   plugins: [react()],
   build: {
     outDir: 'dist', // or any other directory you wish to output your build files
