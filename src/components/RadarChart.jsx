b044bf929700a5938465e206e607617eeae9d643
Will <willtmatteson@gmail.com>
2024-01-25 22:13:31 -0800

    Skills update

--- src/components/RadarChart.jsx
+++ src/components/RadarChart.jsx
@@ -1,28 +1,57 @@
-import  { useState } from 'react';
+import { useState } from 'react';
 import ReactApexChart from 'react-apexcharts';
 
 const RadarChart = () => {
     const [chartOptions, setChartOptions] = useState({
         chart: {
-            height: 350,
+            height: 800,
             type: 'radar',
         },
         title: {
+            margin: 0, // Smaller value reduces space below the title
+            offsetY: 0,
             text: 'Skill tree',
+            style: {
+                fontFamily: 'MS Windows 95', // Set font for title
+            },
         },
         xaxis: {
-            categories: ['Game monetization', 'Game services', 'Docker', 'EVM'],
+            categories: ['Product', 'Coding', 'Growth', 'Mandarin', 'Blockchains', 'Game design', 'Game Economics', 'Docker', 'Game services' ],
+            labels: {
+                style: {
+                    fontFamily: 'MS Windows 95', // Set font for x-axis labels
+                },
+            },
         },
+        yaxis: {
+            tickAmount:4,
+            stepsize: 25,
+            max:100,
+            min:0,
+            labels: {
+                style: {
+                    fontFamily: 'MS Windows 95', // Set font for x-axis labels
+                },
+            },
+        },
+        grid: {
+            padding: {
+                bottom: -20, // Reduce bottom padding
+                top:-20
+                // Other padding options...
+            },
+            // Other grid options...
+        },
+
+        
     });
 
     const [chartSeries, setChartSeries] = useState([
         {
             name: 'Series 1',
-            data: [90, 70, 60, 99],
-            yaxis: {
-                max: 100
-            }
+            data: [99, 30, 70, 35, 99, 80, 90, 50, 90],
         },
+        // ... other series data
     ]);
 
     return (
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -8,32 +8,22 @@ const Styledli = styled.li`
 const Styledh5 = styled.h5`
   padding: 0px !important;
   margin: 0px 0px !important;
+  font-size: 12px;
 `
 
 const Skills = () => {
+    const interests = ['Skiing', 'MOBAs','Film', 'Backpacking','Cooking (Fermentation)', 'William Gibson Novels, OSS']; // Array of interests
+
     return (
         <div>
-
             <RadarChart />
             <div>
-                <h4>
-                    Interests
-                </h4>
-                <ul>
-                    <Styledli>
-                        <Styledh5>Lorem ipsum </Styledh5>
-                    </Styledli>
-                    <Styledli>
-                        <Styledh5>Lorem ipsum </Styledh5>
-                    </Styledli>
-                    <Styledli>
-                        <Styledh5>Lorem ipsum </Styledh5>
-                    </Styledli>
-                </ul>
+                <h5>
+                    Interests:
+                </h5>
+                <Styledh5>{interests.join(', ')}</Styledh5> {/* Join array elements with a comma and space */}
             </div>
-
         </div>
-    )
+    );
 }
-
 export default Skills
